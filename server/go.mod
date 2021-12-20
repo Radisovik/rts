66165b5f0690971c18a1d4a34402b43e9da969ca
Eric Hubbard <eric.hubbars@hpe.com>
2021-12-20 15:48:44 -0800

    Works both ways

--- server/go.mod
+++ server/go.mod
@@ -4,4 +4,7 @@ go 1.18
 
 require github.com/gorilla/websocket v1.4.2
 
-require google.golang.org/protobuf v1.27.1
+require (
+	github.com/golang/protobuf v1.5.0
+	google.golang.org/protobuf v1.26.0 // indirect
+)
--- server/rts.go
+++ server/rts.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"embed"
+	"fmt"
+	proto "github.com/golang/protobuf/proto"
 	"github.com/gorilla/websocket"
-	"google.golang.org/protobuf/proto"
 	"io/fs"
 	"log"
 	"net/http"
-	"server/messages"
+	"os"
+	"time"
 )
 
 //go:embed public/*
@@ -15,28 +17,59 @@ var webRoot embed.FS
 var upgrader = websocket.Upgrader{} // use default options
 
 func main() {
-	htmlContent, err := fs.Sub(webRoot, "public")
-	chk(err)
-	rootFileServer := http.FileServer(http.FS(htmlContent))
-	http.Handle("/", rootFileServer)
+	setupWebHandler()
+
 	http.HandleFunc("/ws", gameConnection)
 	log.Printf("Staring web server")
-	err = http.ListenAndServe(":8000", nil)
+	err := http.ListenAndServe(":8000", nil)
 	chk(err)
 }
 
+func setupWebHandler() {
+	if exists("public") {
+		log.Printf("Noticed public directory -- will serve that!")
+		fs := http.FileServer(http.Dir("./public"))
+		http.Handle("/", fs)
+	} else {
+		log.Printf("No public directory found -- will serve embedded files")
+		htmlContent, err := fs.Sub(webRoot, "public")
+		chk(err)
+		rootFileServer := http.FileServer(http.FS(htmlContent))
+		http.Handle("/", rootFileServer)
+	}
+
+}
+
+func exists(path string) bool {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return false
+	}
+	return true
+}
+
 func gameConnection(w http.ResponseWriter, r *http.Request) {
+	var currentUser = ""
+	var info = func(f string, args ...any) {
+		msg := fmt.Sprintf(f, args...)
+		log.Printf("[%v] %s", currentUser, msg)
+	}
+
 	c, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Printf("Failed to upgrade to a websocket: %v", err)
+		info("Failed to upgrade to a websocket: %v", err)
 		return
 	}
 	defer c.Close()
-	log.Printf("New inbound websocket connection from %v", r.RemoteAddr)
-	fs := &messages.FromServer{
-		Type:  messages.FromServer_Hello,
-		Hello: &messages.Hello{Greeting: "hello world"},
+	defer func() {
+		info("Closing")
+	}()
+	currentUser = r.RemoteAddr
+	info("New inbound websocket connection from %v", currentUser)
+	fs := &FromServer{
+		Epoch: uint64(time.Now().Unix()),
+		Msg:   &FromServer_Hello{Hello: &Hello{Greeting: "Hello from Server"}},
 	}
+
 	marshal, err := proto.Marshal(fs)
 	chk(err)
 	err = c.WriteMessage(websocket.BinaryMessage, marshal)
@@ -44,14 +77,21 @@ func gameConnection(w http.ResponseWriter, r *http.Request) {
 	for {
 		mt, message, err := c.ReadMessage()
 		if err != nil {
-			log.Printf("Error while reading websocket msg")
+			info("Error while reading websocket msg")
 			break
 		}
 		if mt != websocket.BinaryMessage {
-			log.Printf("Someone sent an ASCII message.. we don't do that")
+			info("Someone sent an ASCII message.. we don't do that")
 			break
 		}
-		log.Printf("Here is our cool message: %v", message)
+		fc := FromClient{}
+		err = proto.Unmarshal(message, &fc)
+		if err != nil {
+			info(err.Error())
+			return
+		}
+		info("Here is our cool message: %v", fc.GetHello())
+
 	}
 }
 
